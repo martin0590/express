82a55e161991ebe7bdd01982c4f0af0d3764c260
martin0590 <109174407+martin0590@users.noreply.github.com>
2025-11-04 14:55:35 -0300

    updated index-2

--- index-2.js
+++ index-2.js
@@ -1,6 +1,11 @@
 const express = require('express')
-
+const path = require(path)
 const app = express()
+
+app.use(express.json())
+app.use(express.static('./static')) // with this, everything that is inside the static folder will be available from the browser
+app.use('/public',express.static('./static')) // same as above but i need to go to /public to access its static files
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.get('/products', (req, res) => {
     //validate data
